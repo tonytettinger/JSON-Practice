58bd9079416e6927954c323b3a088376cfb3ab49
Antal Tony Tettinger <antal.tettinger@gmail.com>
2018-09-11 00:02:49 +0800

    Video 1-3 practice

--- 2-javascript-project-json-m2-exercise-files/startJS/js/main.js
+++ 2-javascript-project-json-m2-exercise-files/startJS/js/main.js
@@ -1,1 +1,14 @@
 // JavaScript Document
+let start = '{"favColor":"Red", "favSeason":"Spring"}';
+
+let myObject = JSON.parse(start);
+console.log(myObject);
+
+let myString = JSON.stringify(myObject);
+console.log(myString);
+
+let dataOne = '{"first":"Antal", "last":"Tettinger", "city":"Chengdu"}';
+
+let myData = JSON.parse(dataOne);
+
+document.getElementById('message').innerHTML = myData.last;
